874465fe28235b6a9e7158a990d075dba5f778a3
Andre Almeida <andreluas.k@gmail.com>
2022-01-09 16:32:32 -0300

    initial :tada:

---
+++ src/main/java/com/devsuperior/movieflix/dto/UserDTO.java
@@ -0,0 +1,70 @@
+package com.devsuperior.movieflix.dto;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import com.devsuperior.movieflix.entities.User;
+
+public class UserDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    @NotBlank(message = "Campo obrigatório")
+    private String name;
+
+    @Email(message = "Favor entrar um e-mail válido")
+    private String email;
+
+    Set<RoleDTO> roles = new HashSet<>();
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public UserDTO(User entity) {
+        id = entity.getId();
+        name = entity.getName();
+        email = entity.getEmail();
+        entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+    }
+
+    // #region
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return name;
+    }
+
+    public void setFirstName(String firstName) {
+        this.name = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<RoleDTO> getRoles() {
+        return roles;
+    }   
+    // #endregion
+}
