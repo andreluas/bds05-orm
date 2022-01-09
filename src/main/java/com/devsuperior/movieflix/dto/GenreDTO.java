8a492242dca27b264492a469ad15b923f833232a
Andre Almeida <andreluas.k@gmail.com>
2022-01-09 16:48:58 -0300

    dtos

---
+++ src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
@@ -0,0 +1,50 @@
+package com.devsuperior.movieflix.dto;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
+
+public class GenreDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    Set<Movie> movies = new HashSet<>();
+
+    public GenreDTO() {
+    }
+
+    public GenreDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public GenreDTO(Genre entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Movie> getMovies() {
+        return movies;
+    }
+
+}
